c633835aae4b580d4b7bc34dc9c8a86af8927cd1
Eric Hop <git@bugfree.be>
2020-11-29 15:31:45 -0800

    Prepare wasmclient for direct testing of go code

--- client/host.go
+++ client/host.go
@@ -15,64 +15,44 @@ const (
 )
 
 type Host interface {
-	GetBytes(objId int32, keyId int32, value *byte, size int32) int32
-	GetIntRef(objId int32, keyId int32, value *int64)
-	GetKeyId(key *byte, size int32) int32
+	Exists(objId int32, keyId int32) bool
+	GetBytes(objId int32, keyId int32) []byte
+	GetInt(objId int32, keyId int32) int64
+	GetKey(bytes []byte) int32
+	GetKeyId(key string) int32
 	GetObjectId(objId int32, keyId int32, typeId int32) int32
-	SetBytes(objId int32, keyId int32, value *byte, size int32)
-	SetIntRef(objId int32, keyId int32, value *int64)
+	GetString(objId int32, keyId int32) string
+	SetBytes(objId int32, keyId int32, value []byte)
+	SetInt(objId int32, keyId int32, value int64)
+	SetString(objId int32, keyId int32, value string)
 }
 
-var host Host
-
 // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\ // \\
+
+var host Host
 
 func ConnectHost(h Host) {
 	host = h
 }
 
 func Exists(objId int32, keyId int32) bool {
-	return host.GetBytes(objId, keyId, nil, 0) >= 0
+	return host.Exists(objId, keyId)
 }
 
 func GetBytes(objId int32, keyId int32) []byte {
-	// first query length of bytes array
-	size := host.GetBytes(objId, keyId, nil, 0)
-	if size <= 0 {
-		return []byte(nil)
-	}
-
-	// allocate a byte array in Wasm memory and
-	// copy the actual data bytes to Wasm byte array
-	bytes := make([]byte, size)
-	host.GetBytes(objId, keyId, &bytes[0], size)
-	return bytes
+	return host.GetBytes(objId, keyId)
 }
 
 func GetInt(objId int32, keyId int32) int64 {
-	// Go's Wasm implementation is still geared towards Javascript,
-	// which does not know int64. So instead of calling host.GetInt()
-	// we call host.GetIntRef() with a 32-bit reference to an int64
-	value := int64(0)
-	host.GetIntRef(objId, keyId, &value)
-	return value
+	return host.GetInt(objId, keyId)
 }
 
 func GetKey(bytes []byte) int32 {
-	size := int32(len(bytes))
-	if size == 0 {
-		return host.GetKeyId(nil, -1)
-	}
-	return host.GetKeyId(&bytes[0], -size-1)
+	return host.GetKey(bytes)
 }
 
 func GetKeyId(key string) int32 {
-	bytes := []byte(key)
-	size := int32(len(bytes))
-	if size == 0 {
-		return host.GetKeyId(nil, 0)
-	}
-	return host.GetKeyId(&bytes[0], size)
+	return host.GetKeyId(key)
 }
 
 func GetObjectId(objId int32, keyId int32, typeId int32) int32 {
@@ -80,32 +60,17 @@ func GetObjectId(objId int32, keyId int32, typeId int32) int32 {
 }
 
 func GetString(objId int32, keyId int32) string {
-	// convert UTF8-encoded bytes array to string
-	// negative object id indicates to host that this is a string
-	bytes := GetBytes(-objId, keyId)
-	if bytes == nil {
-		return ""
-	}
-	return string(bytes)
+	return host.GetString(objId, keyId)
 }
 
 func SetBytes(objId int32, keyId int32, value []byte) {
-	var ptr *byte = nil
-	if len(value) != 0 {
-		ptr = &value[0]
-	}
-	host.SetBytes(objId, keyId, ptr, int32(len(value)))
+	host.SetBytes(objId, keyId, value)
 }
 
 func SetInt(objId int32, keyId int32, value int64) {
-	// Go's Wasm implementation is still geared towards Javascript,
-	// which does not know int64. So instead of calling host.SetInt()
-	// we call host.SetIntRef() with a 32-bit reference to the int64
-	host.SetIntRef(objId, keyId, &value)
+	host.SetInt(objId, keyId, value)
 }
 
 func SetString(objId int32, keyId int32, value string) {
-	// convert string to UTF8-encoded bytes array
-	// negative object id indicates to host that this is a string
-	SetBytes(-objId, keyId, []byte(value))
+	host.SetString(objId, keyId, value)
 }
--- wasmclient/host.go
+++ wasmclient/host.go
@@ -37,26 +37,81 @@ func ConnectWasmHost() {
 	client.ConnectHost(WasmHost{})
 }
 
-func (w WasmHost) GetBytes(objId int32, keyId int32, value *byte, size int32) int32 {
-	return hostGetBytes(objId, keyId, value, size)
+func (w WasmHost) Exists(objId int32, keyId int32) bool {
+	return hostGetBytes(objId, keyId, nil, 0) >= 0
 }
 
-func (w WasmHost) GetIntRef(objId int32, keyId int32, value *int64) {
-	hostGetIntRef(objId, keyId, value)
+func (w WasmHost) GetBytes(objId int32, keyId int32) []byte {
+	// first query length of bytes array
+	size := hostGetBytes(objId, keyId, nil, 0)
+	if size <= 0 {
+		return []byte(nil)
+	}
+
+	// allocate a byte array in Wasm memory and
+	// copy the actual data bytes to Wasm byte array
+	bytes := make([]byte, size)
+	hostGetBytes(objId, keyId, &bytes[0], size)
+	return bytes
 }
 
-func (w WasmHost) GetKeyId(key *byte, size int32) int32 {
-	return hostGetKeyId(key, size)
+func (w WasmHost) GetInt(objId int32, keyId int32) int64 {
+	// Go's Wasm implementation is still geared towards Javascript,
+	// which does not know int64. So instead of calling hostGetInt()
+	// we call hostGetIntRef() with a 32-bit reference to an int64
+	value := int64(0)
+	hostGetIntRef(objId, keyId, &value)
+	return value
+}
+
+func (w WasmHost) GetKey(bytes []byte) int32 {
+	size := int32(len(bytes))
+	if size == 0 {
+		return hostGetKeyId(nil, -1)
+	}
+	return hostGetKeyId(&bytes[0], -size-1)
+}
+
+func (w WasmHost) GetKeyId(key string) int32 {
+	bytes := []byte(key)
+	size := int32(len(bytes))
+	if size == 0 {
+		return hostGetKeyId(nil, 0)
+	}
+	return hostGetKeyId(&bytes[0], size)
 }
 
 func (w WasmHost) GetObjectId(objId int32, keyId int32, typeId int32) int32 {
 	return hostGetObjectId(objId, keyId, typeId)
 }
 
-func (w WasmHost) SetBytes(objId int32, keyId int32, value *byte, size int32) {
-	hostSetBytes(objId, keyId, value, size)
+func (w WasmHost) GetString(objId int32, keyId int32) string {
+	// convert UTF8-encoded bytes array to string
+	// negative object id indicates to host that this is a string
+	bytes := w.GetBytes(-objId, keyId)
+	if bytes == nil {
+		return ""
+	}
+	return string(bytes)
 }
 
-func (w WasmHost) SetIntRef(objId int32, keyId int32, value *int64) {
-	hostSetIntRef(objId, keyId, value)
+func (w WasmHost) SetBytes(objId int32, keyId int32, value []byte) {
+	var ptr *byte = nil
+	if len(value) != 0 {
+		ptr = &value[0]
+	}
+	hostSetBytes(objId, keyId, ptr, int32(len(value)))
+}
+
+func (w WasmHost) SetInt(objId int32, keyId int32, value int64) {
+	// Go's Wasm implementation is still geared towards Javascript,
+	// which does not know int64. So instead of calling hostSetInt()
+	// we call hostSetIntRef() with a 32-bit reference to the int64
+	hostSetIntRef(objId, keyId, &value)
+}
+
+func (w WasmHost) SetString(objId int32, keyId int32, value string) {
+	// convert string to UTF8-encoded bytes array
+	// negative object id indicates to host that this is a string
+	w.SetBytes(-objId, keyId, []byte(value))
 }
