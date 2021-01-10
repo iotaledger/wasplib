8ac4532cde116b836d713de97ef11ee37ac1ed8d
lunfardo314 <evaldas.drasutis@iota.org>
2021-01-10 18:34:41 +0200

    tutorial examples

--- rust/contracts/examples/example1/test/solo_test.go
+++ rust/contracts/examples/example1/test/solo_test.go
@@ -35,15 +35,13 @@ func TestSolo2(t *testing.T) {
 
 func TestSolo3(t *testing.T) {
 	env := solo.New(t, false, false)
-	chain := env.NewChain(nil, "ex1")
+	chain := env.NewChain(nil, "ex3")
 	// deploy the contract on chain
 	err := chain.DeployWasmContract(nil, "example1", "../pkg/example1_bg.wasm")
 	require.NoError(t, err)
 
 	// call contract to store string
-	theString := "Hello, world!"
-	req := solo.NewCall("example1", "storeString",
-		"paramString", theString)
+	req := solo.NewCall("example1", "storeString", "paramString", "Hello, world!")
 	_, err = chain.PostRequest(req, nil)
 	require.NoError(t, err)
 
@@ -53,12 +51,12 @@ func TestSolo3(t *testing.T) {
 	returnedString, exists, err := codec.DecodeString(res.MustGet("paramString"))
 	require.NoError(t, err)
 	require.True(t, exists)
-	require.EqualValues(t, theString, returnedString)
+	require.EqualValues(t, "Hello, world!", returnedString)
 }
 
 func TestSolo4(t *testing.T) {
 	env := solo.New(t, false, false)
-	chain := env.NewChain(nil, "ex1")
+	chain := env.NewChain(nil, "ex4")
 	// deploy the contract on chain
 	err := chain.DeployWasmContract(nil, "example1", "../pkg/example1_bg.wasm")
 	require.NoError(t, err)
@@ -67,5 +65,104 @@ func TestSolo4(t *testing.T) {
 	req := solo.NewCall("example1", "storeString")
 	_, err = chain.PostRequest(req, nil)
 	require.Error(t, err)
+}
 
+func TestSolo5(t *testing.T) {
+	env := solo.New(t, false, false)
+	chain := env.NewChain(nil, "ex5")
+
+	// create wallet with 1337 iotas.
+	// wallet has address and it is globally identified through
+	// universal identifier: the agent ID
+	userWallet := env.NewSignatureSchemeWithFunds()
+	userAddress := userWallet.Address()
+	userAgentID := coretypes.NewAgentIDFromAddress(userAddress)
+
+	env.AssertAddressBalance(userAddress, balance.ColorIOTA, 1337) // 1337 on address
+	chain.AssertAccountBalance(userAgentID, balance.ColorIOTA, 0)  // empty on-chain
+
+	t.Logf("Address of the userWallet is: %s", userAddress)
+	numIotas := env.GetAddressBalance(userAddress, balance.ColorIOTA)
+	t.Logf("balance of the userWallet is: %d iota", numIotas)
+	env.AssertAddressBalance(userAddress, balance.ColorIOTA, 1337)
+
+	// send 42 iotas from wallet to own account on-chain, controlled by the same wallet
+	req := solo.NewCall("accounts", "deposit").
+		WithTransfer(balance.ColorIOTA, 42)
+	_, err := chain.PostRequest(req, userWallet)
+	require.NoError(t, err)
+
+	// check address balance: must be 43 (!) iotas less
+	env.AssertAddressBalance(userAddress, balance.ColorIOTA, 1337-43)
+	// check the on-chain account. Must contain 43 (!) iotas
+	chain.AssertAccountBalance(userAgentID, balance.ColorIOTA, 43)
+
+	// withdraw back all iotas
+	req = solo.NewCall("accounts", "withdraw")
+	_, err = chain.PostRequest(req, userWallet)
+	require.NoError(t, err)
+
+	// we are back to initial situation: IOTA is fee-less!
+	env.AssertAddressBalance(userAddress, balance.ColorIOTA, 1337)
+	chain.AssertAccountBalance(userAgentID, balance.ColorIOTA, 0) // empty
+}
+
+func TestSolo6(t *testing.T) {
+	env := solo.New(t, false, false)
+	chain := env.NewChain(nil, "ex6")
+
+	err := chain.DeployWasmContract(nil, "example1", "../pkg/example1_bg.wasm")
+	require.NoError(t, err)
+
+	// global ID of the deployed contract
+	contractID := coretypes.NewContractID(chain.ChainID, coretypes.Hn("example1"))
+	// contract id in the form of the agent ID
+	contractAgentID := coretypes.NewAgentIDFromContractID(contractID)
+
+	userWallet := env.NewSignatureSchemeWithFunds()
+	userAddress := userWallet.Address()
+	userAgentID := coretypes.NewAgentIDFromAddress(userWallet.Address())
+
+	env.AssertAddressBalance(userAddress, balance.ColorIOTA, 1337)
+	chain.AssertAccountBalance(contractAgentID, balance.ColorIOTA, 0) // empty on-chain
+	chain.AssertAccountBalance(userAgentID, balance.ColorIOTA, 0)     // empty on-chain
+
+	req := solo.NewCall("example1", "storeString", "paramString", "Hello, world!").
+		WithTransfer(balance.ColorIOTA, 42)
+	_, err = chain.PostRequest(req, userWallet)
+	require.NoError(t, err)
+
+	chain.AssertAccountBalance(contractAgentID, balance.ColorIOTA, 42)
+	chain.AssertAccountBalance(userAgentID, balance.ColorIOTA, 1)
+	env.AssertAddressBalance(userAddress, balance.ColorIOTA, 1337-43)
+}
+
+func TestSolo7(t *testing.T) {
+	env := solo.New(t, false, false)
+	chain := env.NewChain(nil, "ex7")
+
+	err := chain.DeployWasmContract(nil, "example1", "../pkg/example1_bg.wasm")
+	require.NoError(t, err)
+
+	// global ID of the deployed contract
+	contractID := coretypes.NewContractID(chain.ChainID, coretypes.Hn("example1"))
+	// contract id in the form of the agent ID
+	contractAgentID := coretypes.NewAgentIDFromContractID(contractID)
+
+	userWallet := env.NewSignatureSchemeWithFunds()
+	userAddress := userWallet.Address()
+	userAgentID := coretypes.NewAgentIDFromAddress(userWallet.Address())
+
+	env.AssertAddressBalance(userAddress, balance.ColorIOTA, 1337)
+	chain.AssertAccountBalance(contractAgentID, balance.ColorIOTA, 0) // empty on-chain
+	chain.AssertAccountBalance(userAgentID, balance.ColorIOTA, 0)     // empty on-chain
+
+	req := solo.NewCall("example1", "storeString").
+		WithTransfer(balance.ColorIOTA, 42)
+	_, err = chain.PostRequest(req, userWallet)
+	require.Error(t, err)
+
+	chain.AssertAccountBalance(contractAgentID, balance.ColorIOTA, 0)
+	chain.AssertAccountBalance(userAgentID, balance.ColorIOTA, 1)
+	env.AssertAddressBalance(userAddress, balance.ColorIOTA, 1337-1)
 }
