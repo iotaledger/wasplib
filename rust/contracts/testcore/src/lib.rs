14ca9b8c2f33845cf2dd9d116323718e53999258
lunfardo314 <evaldas.drasutis@iota.org>
2021-01-16 22:21:54 +0200

    more sandbox tests

--- rust/contracts/testcore/src/lib.rs
+++ rust/contracts/testcore/src/lib.rs
@@ -8,6 +8,10 @@ const PARAM_INT_PARAM_VALUE: &str = "intParamValue";
 // const PARAM_HNAME: &str = "hname";
 // const PARAM_CALL_OPTION: &str = "callOption";
 
+const MSG_FULL_PANIC: &str = "========== panic FULL ENTRY POINT =========";
+const MSG_VIEW_PANIC: &str = "========== panic VIEW =========";
+
+const SELF_NAME: &str = "test_sandbox";   // temporary, until hname in the call will become available
 
 #[no_mangle]
 fn on_load() {
@@ -18,6 +22,13 @@ fn on_load() {
     exports.add_call("setInt", set_int);
     exports.add_view("getInt", get_int);
     exports.add_view("fibonacci", fibonacci);
+
+    exports.add_call("testPanicFullEP", test_panic_full_ep);
+    exports.add_view("testPanicViewEP", test_panic_view_ep);
+    exports.add_call("testCallPanicFullEP", test_call_panic_full_ep);
+    exports.add_call("testCallPanicViewEPFromFull", test_call_panic_view_from_full);
+    exports.add_view("testCallPanicViewEPFromView", test_call_panic_view_from_view);
+
 }
 
 fn on_init(ctx: &ScCallContext) {
@@ -67,15 +78,53 @@ fn call_on_chain(ctx: &ScCallContext) {
 }
 
 fn fibonacci(ctx: &ScViewContext) {
-    ctx.log("testcore.fibonacci.begin");
-    //
-    // let n = ctx.params().get_int(PARAM_INT_PARAM_VALUE);
-    // if !n.exists(){
-    //     ctx.panic("param value not found")
-    // }
-    // let n = n.value();
-    // if n == 0 || n == 1{
-    //     ctx.results().get_int(PARAM_INT_PARAM_VALUE).set_value(n);
-    //     return;
-    // }
+    let n = ctx.params().get_int(PARAM_INT_PARAM_VALUE);
+    if !n.exists(){
+        ctx.panic("param value not found")
+    }
+    let n = n.value();
+    ctx.log(&("fibonacci: ".to_string() + &n.to_string()));
+    if n == 0 || n == 1{
+        ctx.log("return 1");
+        ctx.results().get_int(PARAM_INT_PARAM_VALUE).set_value(n);
+        return;
+    }
+    ctx.log("before call 1");
+    let view_call1 = ctx.view("fibonacci");
+    view_call1.contract(SELF_NAME);
+    view_call1.params().get_int(PARAM_INT_PARAM_VALUE).set_value(n-1);
+    view_call1.view();
+    let n1 = view_call1.results().get_int(PARAM_INT_PARAM_VALUE).value();
+    ctx.log(&("    fibonacci-1: ".to_string() + &n1.to_string()));
+
+    let view_call2 = ctx.view("fibonacci");
+    view_call2.contract(SELF_NAME);
+    view_call2.params().get_int(PARAM_INT_PARAM_VALUE).set_value(n-2);
+    view_call2.view();
+    let n2 = view_call2.results().get_int(PARAM_INT_PARAM_VALUE).value();
+    ctx.log(&("    fibonacci-2: ".to_string() + &n2.to_string()));
+
+    ctx.results().get_int(PARAM_INT_PARAM_VALUE).set_value(n1+n2);
+}
+
+fn test_panic_full_ep(ctx: &ScCallContext){
+    ctx.panic(MSG_FULL_PANIC)
+}
+
+fn test_panic_view_ep(ctx: &ScViewContext){
+    ctx.panic(MSG_VIEW_PANIC)
+}
+
+fn test_call_panic_full_ep(ctx: &ScCallContext){
+    ctx.call("testPanicFullEP").contract(SELF_NAME).call();  // FIXME need self.hname
+}
+
+// FIXME no need for 'view method special'
+fn test_call_panic_view_from_full(ctx: &ScCallContext){
+    ctx.view("testPanicViewEP").contract(SELF_NAME).view();  // FIXME need self.hname
+}
+
+// FIXME no need for 'view method special'
+fn test_call_panic_view_from_view(ctx: &ScViewContext){
+    ctx.view("testPanicViewEP").contract(SELF_NAME).view();
 }
