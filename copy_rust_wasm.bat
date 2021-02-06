75b16c5218086250bfe0f414628f29210cbd0019
Eric Hop <git@bugfree.be>
2021-02-05 20:49:34 -0800

    Removed obsolete batch files

--- copy_rust_wasm.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-if not exist wasm md wasm
-cd rust\contracts
-for /D %%f in (*.) do if exist %%f\pkg\%%f_bg.wasm copy /Y %%f\pkg\%%f_bg.wasm ..\..\wasm
-cd ..\..
-
-
--- java/src/org/iota/wasplib/contracts/template.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-if not "%1"=="" goto :makeTemplate
-echo Please specify smart contract name
-goto :xit
-:makeTemplate
-copy HelloWorld.java %1.java
-grepl -i HelloWorld %1 %1.java
-:xit
